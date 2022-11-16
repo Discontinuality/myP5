b6a47817748a11c48738363b9ecae2d96f020e5b
Stephen Johnson <discontinuality@gmail.com>
2022-11-16 15:55:05 +0000

    Start random walk

---
+++ random-walks/sketch.js
@@ -0,0 +1,39 @@
+let walker;
+
+function setup() {
+  createCanvas(640, 360);  // put setup code here
+
+  walker = new Walker();
+  background(220);
+}
+
+function draw() {
+  walker.step();
+  walker.show();
+}
+
+class Walker {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
+  }
+
+  show() {
+    stroke(0);
+    point(this.x, this.y);
+  }
+  
+  step() {
+    let choice = floor(random(4));
+
+    if (choice == 0) {
+      this.x++;
+    } else if (choice == 1) {
+      this.x--;
+    } else if (choice == 2) {
+      this.y++;
+    } else {
+      this.y--;
+    }
+  }
+}
